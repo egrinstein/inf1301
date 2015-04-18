daa0820252ebe7994c848edc833a403eb9aa4252
Leonardo Edelman Wajnsztok <leotok@hotmail.com>
2015-04-18 12:52:31 -0300

    pop e destruir

--- trabalho-2/Modulo-Monte/MONTE.c
+++ trabalho-2/Modulo-Monte/MONTE.c
@@ -1,15 +1,4 @@
 /***************************************************************************
-*  $MCI Módulo de implementação: MON  Monte de cartas
-*
-*  Arquivo gerado:              MONTE.c
-*  Letras identificadoras:      MON
-*
-*  Nome da base de software:    Arcabouço para a automação de testes de programas redigidos em C
-*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\LISTA.BSW
-*
-*  Projeto: INF 1301 Paciência Spider
-*  Autores: 
-*/***************************************************************************
  *  $MCI Módulo de implementação: MON Monte de cartas
  *
  *  Arquivo gerado:              MONTE.c
@@ -88,7 +77,12 @@
 *  ****/
    MON_tpCondRet MON_DestruirMonte ( MON_tppMonte pMonte )
    {
-      PIL_DestroiPilha( pMonte );
+
+      PIL_tppPilha pPilha = ( PIL_tppPilha ) pMonte ;
+
+      PIL_DestroiPilha( pPilha );
+
+      return MON_CondRetOK ; 
    }
 
   /* Fim função: MON  &Destruir monte */
@@ -100,75 +94,11 @@
 
    MON_tpCondRet MON_PopCartaMonte ( MON_tppMonte pMonte, CAR_tppCarta * pCarta ) 
    {
-      PIL_PopCarta( pMonte, pCarta );
-   }
-  
-   /* Fim função: MON  &Pop carta do monte */
+      PIL_tppPilha pPilha = ( PIL_tppPilha ) pMonte ;
 
-/***************************************************************************
-*
-*  Função: LIS  &Inserir elemento antes
-*  ****/
+      PIL_PopCarta( pPilha, pCarta );
 
-/********** Fim do módulo de implementação: MON Monte de cartas **********/
-
-*
-***************************************************************************/
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "MONTE.h"
-#include "PILHADECARTAS.h"
-#include "CARTA.h"
-
-/***********************************************************************
-*
-*  $TC Tipo de dados: MON Monte Pilha de Carta
-*
-*
-***********************************************************************/
-
-   typedef PIL_tppPilha MON_tppMonte ;
-
-/*****  Código das funções exportadas pelo módulo  *****/
-
-/***************************************************************************
-*
-*  Função: MON  &Criar monte
-*  ****/
-
-   MON_tppMonte MON_CriarMonte ( MON_tppMonte * pMonte , PIL_tppPilha pPilha)
-   {
-      *pMonte = PIL_CriarPilhaVazia( pPilha );
-       if ( pMonte == NULL)
-       {
-         return NULL;
-       }
-       return pMonte;
-   }
-
-    /* Fim função: MON  &Criar monte */
-
-/***************************************************************************
-*
-*  Função: MON  &Destruir monte
-*  ****/
-   MON_tpCondRet MON_DestruirMonte ( MON_tppMonte pMonte )
-   {
-      PIL_DestroiPilha( pMonte );
-   }
-
-  /* Fim função: MON  &Destruir monte */
-
-/***************************************************************************
-*
-*  Função: MON  &Pop carta do monte
-*  ****/
-
-   MON_tpCondRet MON_PopCartaMonte ( MON_tppMonte pMonte, CAR_tppCarta * pCarta ) 
-   {
-      PIL_PopCarta( pMonte, pCarta );
+      return MON_CondRetOK ; 
    }
   
    /* Fim função: MON  &Pop carta do monte */
