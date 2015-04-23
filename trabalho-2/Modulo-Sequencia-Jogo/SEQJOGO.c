2aff0981bc725b4b63333d0931202f0724805247
Leonardo Wajnsztok <leotok@hotmail.com>
2015-04-23 20:01:46 -0300

    condicoes de mover melhoradas

--- trabalho-2/Modulo-Sequencia-Jogo/SEQJOGO.c
+++ trabalho-2/Modulo-Sequencia-Jogo/SEQJOGO.c
@@ -44,6 +44,14 @@
     
 } SEQJ_tpSeqJogo ;
 
+/***** Protótipos das funções encapuladas no módulo *****/
+
+int totalCartasNaSeq( SEQJ_tppSeqJ pSeqJ );
+
+int ehSequenciaValor( CAR_tppCarta carta1, CAR_tppCarta carta2 ) ;
+
+int ehMesmoNaipe( CAR_tppCarta carta1, CAR_tppCarta carta2 ) ;
+
 /*****  Código das funções exportadas pelo módulo  *****/
 
 /***************************************************************************
@@ -167,12 +175,6 @@
     CAR_tppCarta cartaAux;
     CAR_tppCarta cartaAnterior;
 
-    char naipeAtual = '?' ;
-    char naipeAnterior = '!' ; 
-
-    int valorAtual = 0 ; 
-    int valorAnterior = 0 ;
-
     int numCartasSeq1 = 0;
     int seqComecou = 0;
     int i = 0;
@@ -181,70 +183,61 @@
     // 2 ver se podem se colocadas na seq2
     // 3 move-las
 
-    //faltou verificar se cartas estao viradas
-    // e dps virar carta, se for o caso
-
     if( numCartas > pSeqJ1->numCartasViradas)   // verifica caso de pedir mais cartas do que o numero de cartas viradas
     {
       return SEQJ_CondRetNumCartasIncorreto;
     }
 
-    while( PIL_VerCarta( pSeqJ1->pPilha, &cartaAux, numCartasSeq1 ) == PIL_CondRetOK )
-    {
-      numCartasSeq1++;
-    }
+   numCartasSeq1 = totalCartasNaSeq( pSeqJ1 );  //numero de cartas inicial da seq1
 
-    if( numCartasSeq1 < numCartas )  // verifica se existem n numCartas na pilha a ser movida
-    {
-      return SEQJ_CondRetNumCartasIncorreto;
-    }
 
-    while( i < numCartas)   // verifica se as cartas a serem tiradas estão em sequencia
+    for( i=0 ; i < numCartas ; i++)   // verifica se as cartas a serem tiradas estão em sequencia e sao mesmo naipe
     {
       PIL_VerCarta( pSeqJ1->pPilha, &cartaAux, i );
 
       if( seqComecou )
       {
-        CAR_ObterNaipe(cartaAux, &naipeAtual);
-        CAR_ObterNaipe(cartaAnterior, &naipeAnterior);
-
-        CAR_ObterValor(cartaAux, &valorAtual);
-        CAR_ObterValor(cartaAnterior, &valorAnterior);
-
-        if( valorAnterior != valorAtual - 1 || naipeAnterior != naipeAtual )
+        if( !ehSequenciaValor(cartaAux, cartaAnterior) || !ehMesmoNaipe(cartaAux, cartaAnterior))
         {
           return SEQJ_CondRetCartasForaDaSequencia;
         }
       }
       cartaAnterior = cartaAux;
       if( !seqComecou ) seqComecou = 1;
-
-      i++;
     }
 
-    i = 0;  //depois de testadas as condições, mover cartas
+    // ver se pode colocar na seq2
+    // ultima carta testada fica em cartaAux, entao comparar cartaAux com a primeira da pseqj2
+    //cartaAnterior deve ser maior do q cartaAux
+
+    PIL_VerCarta( pSeqJ2->pPilha, &cartaAnterior, 0 ); 
+
+    if ( !ehSequenciaValor( cartaAnterior, cartaAux ))
+    {
+        return SEQJ_CondRetCartasForaDaSequencia;
+    }
+
+
+  //depois de testadas as condições, mover cartas
+
     PIL_CriarPilhaVazia( &pilhaAux );
 
-    while( i < numCartas )
+    for( i = 0 ; i < numCartas ; i++ )
     {
       PIL_PopCarta( pSeqJ1, &cartaAux );
       PIL_PushCarta( pilhaAux, cartaAux );
-      i++;
     }
 
-    i = 0;
-
-    while( i < numCartas )
+    for( i = 0 ; i < numCartas ; i++)
     {
       PIL_PopCarta( pilhaAux, &cartaAux );
       PIL_PushCarta( pSeqJ2, cartaAux );
-      i++;
     }
 
     pSeqJ1->numCartasViradas -= numCartas;
-    numCartasSeq1 -= numCartas;
-
     pSeqJ2->numCartasViradas += numCartas;
+
+    numCartasSeq1 -= numCartas;
 
     if( pSeqJ1->numCartasViradas == 0  && numCartasSeq1 > 0)
     {
@@ -256,8 +249,82 @@
     return SEQJ_CondRetOK;
   }
 
-
-
 /* Fim função: SEQJ &Move Pilha de sequência1 para sequência2 */
+
+  /*****  Código das funções encapsuladas no módulo  *****/
+
+
+/***********************************************************************
+ *
+ *  $FC Função: SEQJ  -Total de cartas na sequência
+ *
+ *  $ED Descrição da função
+ *    Dada um sequência, a função conta o total de cartas em sua pilha.
+ *
+ ***********************************************************************/
+
+int totalCartasNaSeq( SEQJ_tppSeqJ pSeqJ )
+{
+  CAR_tppCarta cartaAux ;
+  int numCartasSeq = 0 ;
+
+  while( PIL_VerCarta( pSeqJ->pPilha, &cartaAux, numCartasSeq ) == PIL_CondRetOK )
+  {
+    numCartasSeq++;
+  }
+
+  return numCartasSeq ;
+}
+
+/***********************************************************************
+ *
+ *  $FC Função: SEQJ  Verifica cartas sequência relação ao valor
+ *
+ *  $ED Descrição da função
+ *    Dadas duas cartas, a função verifica se a primeira é sequência da segunda.
+ *    Ex: carta1 = 6 de ouro e carta2 = 5 de ouro é sequencia
+ *    Ex: carta1 = 6 de ouro e carta2 = 5 de copas é sequencia
+ *    Ex: carta1 = 4 de ouro e carta2 = 6 de ouro não é sequencia
+ *
+ ***********************************************************************/
+
+int ehSequenciaValor( CAR_tppCarta carta1, CAR_tppCarta carta2 )
+{
+  int valor1 = 0 ; 
+  int valor2 = 0 ;
+
+   CAR_ObterValor(carta1, &valor1);
+   CAR_ObterValor(carta2, &valor2);
+
+   if( valor1 -1  == valor2 ) 
+   {
+      return 1;   // carta1 é sequencia e maior do que carta2
+   }
+   return 0;
+}
+
+/***********************************************************************
+ *
+ *  $FC Função: SEQJ  Verifica cartas possuem mesmo naipe
+ *
+ *  $ED Descrição da função
+ *    Dadas duas cartas, a função verifica ambas possuem mesmo naipe.
+ *
+ ***********************************************************************/
+
+int ehMesmoNaipe( CAR_tppCarta carta1, CAR_tppCarta carta2 )
+{
+   char naipe1 = '?' ;
+   char naipe2 = '!' ; 
+
+   CAR_ObterNaipe(carta1, &naipe1);
+   CAR_ObterNaipe(carta2, &naipe2);
+
+   if( naipe1 == naipe2 ) 
+   {
+      return 1;
+   }
+   return 0;
+}
 
 /*************** Fim do módulo de implementação: SEQJ Sequência de jogo *****************/
